cc0d1713866af3110c1c9bdb24649cce443cca93
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2023-07-23 22:45:32 +0530

    Add files via upload

---
+++ Hackerrank solutions/#10Nested_lists.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    
+    score_list = []
+    marksheet = []
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        score_list.append(score)
+        marksheet.append([name,score])
+    second_low = sorted(list(set(score_list)))[1]
+    names =[name for name , scores in sorted(marksheet) if scores == second_low]
+    print('\n'.join(names))
