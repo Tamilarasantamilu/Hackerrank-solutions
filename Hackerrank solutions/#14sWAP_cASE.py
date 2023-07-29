a6e8011d6b86abc3a40654db5363405b0f73bebf
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2023-07-29 21:03:57 +0530

    Add files via upload

---
+++ Hackerrank solutions/#14sWAP_cASE.py
@@ -0,0 +1,15 @@
+def swap_case(s):
+    res=''
+    
+    for ch in s:
+        if ch.isupper():
+            res +=ch.lower()
+        else:
+            res +=ch.upper()
+            
+    return res
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
