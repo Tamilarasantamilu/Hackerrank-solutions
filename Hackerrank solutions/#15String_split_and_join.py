929e4ce7fb0ecca4d6e8be2e8a780202737e0d0c
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2023-07-29 21:11:04 +0530

    Add files via upload

---
+++ Hackerrank solutions/#15String_split_and_join.py
@@ -0,0 +1,12 @@
+def split_and_join(line):
+    a=line.split(" ")
+    
+    a="-".join(a)
+    
+    return a
+    # write your code here
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
