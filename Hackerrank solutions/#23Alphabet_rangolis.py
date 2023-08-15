0b3f496a2616ea65f8760b17fa2ca17746ef4112
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2023-08-15 15:47:01 +0530

    Add files via upload

---
+++ Hackerrank solutions/#23Alphabet_rangolis.py
@@ -0,0 +1,19 @@
+def print_rangoli(size):
+    # your code goes here
+    alphabet = [chr(i) for i in range(97, 123)]
+    alphabet = alphabet[:size]
+    indices = list(range(size))
+    indices = indices[:-1] + indices[::-1]
+    for i in indices:
+        start_index = i +1
+        original = alphabet[-start_index:]
+        reverse = original[::-1]
+        row = reverse + original [1:]
+        row = "-".join(row)
+        width = size * 4-3
+        row = row.center(width, "-")
+        print(row)
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
