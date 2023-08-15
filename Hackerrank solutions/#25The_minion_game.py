6058a26d406583a6fb0f606e89f6532980aaa072
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2023-08-15 16:08:58 +0530

    Add files via upload

---
+++ Hackerrank solutions/#25The_minion_game.py
@@ -0,0 +1,20 @@
+def minion_game(string):
+    vowels = 'AEIOU'
+    kevin_scores = stuart_scores = 0
+    length = len(string)
+    for i in range(length):
+        if string[i] in vowels:
+                kevin_scores += length - i
+        else:
+            stuart_scores += length - i
+    if stuart_scores > kevin_scores:
+        print(f'Stuart {stuart_scores}')
+    elif kevin_scores > stuart_scores:
+        print(f'Kevin {kevin_scores}')
+    else:
+        print ('Draw')
+    # your code goes here
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
