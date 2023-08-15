c4f73435ba1a4ace016e23a99a7f4efaa708f8b3
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2023-08-15 16:30:26 +0530

    Add files via upload

---
+++ Hackerrank solutions/#26merge_the_tools.py
@@ -0,0 +1,13 @@
+def merge_the_tools(string, k):
+    new_str = ''
+    for i in range (0, len(string), k):
+        # print(string[i:i+k])
+        for letter in string[i:i+k]:
+            if letter not in new_str:
+                new_str = new_str + letter
+        print(new_str)
+        new_str = ''
+    
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
