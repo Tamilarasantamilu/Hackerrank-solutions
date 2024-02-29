9666d237540590fb9908f79421c4177113b843b5
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2024-03-01 01:00:42 +0530

    Update #26merge_the_tools.py

--- Hackerrank solutions/#26merge_the_tools.py
+++ Hackerrank solutions/#26merge_the_tools.py
@@ -1,7 +1,6 @@
 def merge_the_tools(string, k):
     new_str = ''
-    for i in range (0, len(string), k):
-        # print(string[i:i+k])
+    for i in range (0, len(string), k):
         for letter in string[i:i+k]:
             if letter not in new_str:
                 new_str = new_str + letter
