0036fbbc46af018b5aaad09288da3b52c488a758
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2024-08-23 22:18:16 +0530

    Update #21Designer_door_mat.py

--- Hackerrank solutions/#21Designer_door_mat.py
+++ Hackerrank solutions/#21Designer_door_mat.py
@@ -1,4 +1,4 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-n,m = map(int , input().split())
+n,m = map(int , input().split())
 pattern = [('.|.'*(2*i+1)).center(m, '-') for i in range (n//2)]
 print('\n'.join(pattern + ['WELCOME'.center(m,'-')]+ pattern[::-1]))
+#end code
