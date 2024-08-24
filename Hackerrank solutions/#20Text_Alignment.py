e86a0530484b3d8d31af76d68fad39f54c294000
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2024-08-24 23:13:25 +0530

    Update #20Text_Alignment.py

--- Hackerrank solutions/#20Text_Alignment.py
+++ Hackerrank solutions/#20Text_Alignment.py
@@ -23,4 +23,4 @@ for i in range(thickness+1):
 for i in range(thickness):
     print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
 
-#end
+#end code
