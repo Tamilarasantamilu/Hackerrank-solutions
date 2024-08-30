63e685c1c86d146cac80f6f44a06d85d87493cdf
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2024-08-30 19:36:48 +0530

    Update #22String_formating.py

--- Hackerrank solutions/#22String_formating.py
+++ Hackerrank solutions/#22String_formating.py
@@ -1,4 +1,4 @@
-def print_formatted(number):
+ print_formatted(number):
     l = len(str(bin(number)[2:]))
     for i in range (1, number+1):
         print(str(i).rjust(l) + ' ' + oct(i)[2:].rjust(l) + ' '+ hex(i)[2:].upper().rjust(l) + ' ' + bin(i)[2:].rjust(l))
@@ -7,3 +7,4 @@ def print_formatted(number):
 if __name__ == '__main__':
     n = int(input())
     print_formatted(n)
+#end code
