12e574da0ad5ae4e0e41bef03e30005e1cd2e083
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2024-09-21 22:28:44 +0530

    Update #6Write_a_functions.py

--- Hackerrank solutions/#6Write_a_functions.py
+++ Hackerrank solutions/#6Write_a_functions.py
@@ -1,5 +1,6 @@
-def is_leap(year):
+ is_leap(year):
     leap = False
     if (year % 4==0) and (year %100!=0 or year %400 ==0):
         leap=True    
-    return leap
+    return leap
+#end code
